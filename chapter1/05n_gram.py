7e950394f2a50efb7f04c272e04f2171be65c41c
Koichi Taura <taura.koichi@gmail.com>
2018-10-15 22:03:07 +0900

    [add] problem 5 n gram

---
+++ chapter1/05n_gram.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+def n_gram(n, word):
+    n_gram_words = []
+    for i in range(len(word)):
+        n_gram_words.append(word[i:i+n])
+    return n_gram_words
+
+word = "I am an NLPer"
+print(n_gram(2, word))
