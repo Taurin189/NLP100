269be75abb254e88242ac9b0cdad133a0fbfdfea
Koichi Taura <taura.koichi@gmail.com>
2018-10-17 23:23:46 +0900

    [modify] problem 8 cipher fix correctly

--- chapter1/08ciphertext.py
+++ chapter1/08ciphertext.py
@@ -6,11 +6,13 @@ def cipher(text):
     cipher_text = ""
     for c in text:
         if re.compile(r'[a-z]').match(c) is not None:
-            cipher_text += str(219 - ord(c))
+            cipher_text += chr(219 - ord(c))
         else:
             cipher_text += c
     return cipher_text
 
 
 text = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics"
-print(cipher(text))
+ciphered_text=cipher(text)
+print(ciphered_text)
+print(cipher(ciphered_text))
