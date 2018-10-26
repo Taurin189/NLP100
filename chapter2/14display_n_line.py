50bebfc76787887cee535f0b1f33cbedc55c5feb
Koichi Taura <taura.koichi@gmail.com>
2018-10-26 05:57:45 -0400

    [modify] problem 14 show line number n

--- chapter2/14display_n_line.py
+++ chapter2/14display_n_line.py
@@ -1,13 +1,14 @@
 # coding: utf-8
-import linecache
 
 temperature_data = open("hightemp.txt", "r")
-max_line = len(temperature_data.readlines())
 
 n = int(input())
-if n > max_line or n <= 0:
-    print("invalid input")
-    exit(0)
-print(linecache.getline("hightemp.txt", n))
+i = 0
+for line in temperature_data.readlines():
+    if i >= n:
+        break
+    print(line.replace("\n", " "))
+    i += 1
+temperature_data.close()
 
 
