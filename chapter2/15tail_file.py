d93b1c33fd3da6e8189324b17e623b4991d0872d
Koichi Taura <taura.koichi@gmail.com>
2018-10-27 07:12:30 -0400

    [modify] problem 14 make it simpler

--- chapter2/15tail_file.py
+++ chapter2/15tail_file.py
@@ -3,9 +3,6 @@
 temperature_data = open("hightemp.txt", "r")
 
 n = int(input())
-for line in temperature_data.readlines()[0:n]:
+for line in temperature_data.readlines()[-n:]:
     print(line.replace("\n", " "))
-
 temperature_data.close()
-
-
