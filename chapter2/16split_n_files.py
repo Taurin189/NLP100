cf86feb9c3fe684c79c2c713d310424563e57a08
Koichi Taura <taura.koichi@gmail.com>
2018-10-29 06:32:39 -0400

    [add] problem 16 split file

---
+++ chapter2/16split_n_files.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+import math
+
+temperature_data = open("hightemp.txt", "r")
+line_count= len(temperature_data.readlines())
+temperature_data.close()
+n = int(input())
+one_file_lines = math.floor(line_count / n)
+
+print(line_count)
+print(one_file_lines)
+
+for i in range(n):
+    with open("hightemp." + str(i + 1) + ".txt", "w") as output:
+        temperature_data = open("hightemp.txt", "r")
+        for line in temperature_data.readlines()[i*one_file_lines: (i+1)*one_file_lines]:
+            output.write(line)
+        temperature_data.close()
