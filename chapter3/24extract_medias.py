0518c5354e41ac93496e62ff64d3d79788799461
Koichi Taura <taura.koichi@gmail.com>
2018-11-05 22:09:27 +0900

    [add] problem 24 extract ref name but it doesn't work well if there are more than one refs

---
+++ chapter3/24extract_medias.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+import json
+import re
+
+wiki_json = open("jawiki-country.json", "r")
+for wiki_line in wiki_json:
+    wiki_info = json.loads(wiki_line)
+    wiki_texts = wiki_info['text'].split("\n")
+    for wiki_text in wiki_texts:
+        ref_url = re.search("(?<=<ref>)\w+://\S+(?=</ref>)", wiki_text)
+        if ref_url is not None:
+            print(ref_url.group(0))
