4762c9c09fd40060232bb2913041217586b1cbbf
Koichi Taura <taura.koichi@gmail.com>
2018-11-08 22:59:12 +0900

    [modify] problem 27 simplify

--- chapter3/27delete_inner_link.py
+++ chapter3/27delete_inner_link.py
@@ -8,9 +8,7 @@ for wiki_line in wiki_json:
     wiki_info = json.loads(wiki_line)
     wiki_texts = wiki_info['text'].split("\n")
     for wiki_text in wiki_texts:
-        original_text = wiki_text
-        wiki_text = re.sub(r'\[\[[^\]]+\|([^\]]+)\]\]', r'\1', wiki_text)
-        wiki_text = re.sub(r'\[\[([^\]]+)\]\]', r'\1', wiki_text)
+        wiki_text = re.sub(r'\[\[[^\]]+\|([^\]]+)\]\]|\[\[([^\]]+)\]\]', r'\1\2', wiki_text)
         output.append(wiki_text)
 for line in output:
     print(line)
