870e095b27f77871798fc1c97eebf334716a9aa5
Koichi Taura <taura.koichi@gmail.com>
2018-11-10 15:39:31 +0900

    [modify] extract delete inner link

--- chapter3/27delete_inner_link.py
+++ chapter3/27delete_inner_link.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 import json
-import re
+from chapter3 import delete_markups
 
 wiki_json = open("jawiki-country.json", "r")
 output = []
@@ -8,7 +8,7 @@ for wiki_line in wiki_json:
     wiki_info = json.loads(wiki_line)
     wiki_texts = wiki_info['text'].split("\n")
     for wiki_text in wiki_texts:
-        wiki_text = re.sub(r'\[\[[^\]]+\|([^\]]+)\]\]|\[\[([^\]]+)\]\]', r'\1\2', wiki_text)
+        wiki_text = delete_markups.delete_inner_link(wiki_text)
         output.append(wiki_text)
 for line in output:
     print(line)
--- chapter3/delete_markups.py
+++ chapter3/delete_markups.py
@@ -1,7 +1,13 @@
 # coding: utf-8
 import re
 
+
 def delete_emphasize_markup(text):
     text = text.replace("'''''", "")
     text = text.replace("'''", "")
+    return text
+
+
+def delete_inner_link(text):
+    text = re.sub(r'\[\[[^\]]+\|([^(\]\])]+)\]\]|\[\[([^(\]\])]+)\]\]', r'\1\2', text)
     return text
