83f4d8092338ea52579ff872c0a459bf604cddd0
Koichi Taura <taura.koichi@gmail.com>
2018-11-11 15:37:27 +0900

    [add] delete markups

--- chapter3/27delete_inner_link.py
+++ chapter3/27delete_inner_link.py
@@ -1,14 +1,18 @@
 # coding: utf-8
 import json
+from chapter3 import extract_template
 from chapter3 import delete_markups
 
 wiki_json = open("jawiki-country.json", "r")
-output = []
-for wiki_line in wiki_json:
-    wiki_info = json.loads(wiki_line)
-    wiki_texts = wiki_info['text'].split("\n")
-    for wiki_text in wiki_texts:
-        wiki_text = delete_markups.delete_inner_link(wiki_text)
-        output.append(wiki_text)
-for line in output:
-    print(line)
+output_dict = extract_template.get_country_information(wiki_json)
+new_output_dict = {}
+for country_index, country_dict in output_dict.items():
+    tmp_dict = {}
+    for key, value in country_dict.items():
+        value = delete_markups.delete_inner_link(value)
+        if value.find(']]') > -1:
+            print(value)
+        tmp_dict.update({key: value})
+    new_output_dict.update({country_index: tmp_dict})
+
+print(new_output_dict)
--- chapter3/28delete_mediawiki_markup.py
+++ chapter3/28delete_mediawiki_markup.py
@@ -1,1 +1,18 @@
+# coding: utf-8
+from chapter3 import extract_template
+from chapter3 import delete_markups
 
+wiki_json = open("jawiki-country.json", "r")
+output_dict = extract_template.get_country_information(wiki_json)
+new_output_dict = {}
+for country_index, country_dict in output_dict.items():
+    tmp_dict = {}
+    for key, value in country_dict.items():
+        value = delete_markups.delete_emphasize_markup(value)
+        value = delete_markups.delete_inner_link(value)
+        if value.find("'''") > -1:
+            print(value)
+        tmp_dict.update({key: value})
+    new_output_dict.update({country_index: tmp_dict})
+
+print(new_output_dict)
