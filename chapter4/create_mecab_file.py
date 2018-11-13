536e164e441ea16226a388c876fc0cf7ffb33048
Koichi Taura <taura.koichi@gmail.com>
2018-11-13 23:01:54 +0900

    [add] create_mecab_file

---
+++ chapter4/create_mecab_file.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+import MeCab
+
+m = MeCab.Tagger ("mecabrc")
+original_text_file = open("neko.txt", "r")
+original_text = ""
+for line in original_text_file:
+    original_text += line + "\n"
+
+with open("neko.txt.mecab", "w") as output:
+    output.write(m.parse(original_text))
+
+original_text_file.close()
