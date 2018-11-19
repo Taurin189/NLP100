3cc145282d194b28ce952a9357216871a13ab1bf
Koichi Taura <taura.koichi@gmail.com>
2018-11-19 22:09:20 +0900

    [add] extract original verb

---
+++ chapter4/32extract_verb.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+import re
+
+mecab_file = open("neko.txt.mecab", "r")
+categorized_words = []
+
+for word_line in mecab_file:
+    char = word_line.split("\t")
+    if len(char) >= 4:
+        if re.match("動詞", char[3]) is not None:
+            print(char[2])
