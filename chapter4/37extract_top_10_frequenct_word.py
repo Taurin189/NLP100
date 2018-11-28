74737e4d393a7b1970c11ce8a288a80b1b741fc0
Koichi Taura <taura.koichi@gmail.com>
2018-11-28 22:53:11 +0900

    [modify] problem 37 and show graph with japanese label

--- chapter4/37extract_top_10_frequenct_word.py
+++ chapter4/37extract_top_10_frequenct_word.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 from chapter4 import dict_utils
-import pandas as pd
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 frequency_words_dict = dict_utils.get_frequency_word_dict("neko.txt.mecab")
@@ -8,15 +8,21 @@ frequency_words_dict = sorted(frequency_words_dict.items(), key=lambda x: -x[1])
 print(frequency_words_dict)
 
 frequent_top_10_words = {}
+frequent_top_10_words_list = []
+x = []
+y = []
 count = 0
 for val in frequency_words_dict:
-    frequent_top_10_words.update({list(val)[0]: list(val)[1]})
+    frequent_top_10_words.update({list(val)[1]: list(val)[0]})
     print(list(val))
+    x.append(list(val)[0])
+    y.append(list(val)[1])
     count += 1
     if count >= 10:
         break
-df = pd.DataFrame.from_dict(frequent_top_10_words)
-data = df['score'].tolist()
-data = df.iloc[:0]
-plt.hist(frequent_top_10_words)
+
+font = {"family": "AppleGothic"}
+mpl.rc('font', **font)
+
+plt.plot(x, y)
 plt.show()
