d8d50fd6edaa0a8ff8c9211405dd0b174b3e9200
Koichi Taura <taura.koichi@gmail.com>
2019-01-05 12:48:46 +0900

    [add] 43 problem but need to verify the verb

--- chapter5/43extract_verb_with_noun.py
+++ chapter5/43extract_verb_with_noun.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from chapter5.morph_utils import get_chunk_dict, get_chunk_list
+from chapter5.morph_utils import get_chunk_dict, get_chunk_list, get_chunk_text
 
 chunk_list = get_chunk_list("neko.txt.cabocha", 8)
 chunk_dict = get_chunk_dict("neko.txt.cabocha", 8)
@@ -7,7 +7,6 @@ chunk_dict = get_chunk_dict("neko.txt.cabocha", 8)
 for chunk in chunk_list:
     morph_list_list = chunk.get_morph_list_list()
     for morph_list in morph_list_list:
-        print(morph_list)
         if morph_list[2] == '名詞':
-            print(chunk.get_num())
-            print(chunk.get_dst())
+            tmp_text = ""
+            print(get_chunk_text(tmp_text, chunk, "neko.txt.cabocha", 8))
