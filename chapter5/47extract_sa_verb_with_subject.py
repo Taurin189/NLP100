7672e77af9aec49692148c368913af11467dbf34
Koichi Taura <taura.koichi@gmail.com>
2019-02-11 13:40:40 +0900

    [add]55 problem to extract NNP but couldn't find way to extract person name

---
+++ chapter5/47extract_sa_verb_with_subject.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+from chapter5.morph_utils import get_chunk_dict, get_chunk_list
+
+chunk_list = get_chunk_list("neko.txt.cabocha", 845)
+chunk_dict = get_chunk_dict("neko.txt.cabocha", 845)
+
+verb_dist = {}
+subject_dict = {}
+text_dict = {}
+
+for chunk in chunk_list:
+    morph_list = chunk.get_morph_by_case('動詞')
+    for morph in morph_list:
+        verb_dist.update({chunk.get_num(): morph.base})
+        tmp_text = morph_list[0]
+        break
+
+for k, v in verb_dist.items():
+    tmp_text = v + "\t"
+    for chunk in chunk_list:
+        if chunk.get_dst() == k:
+            morph_list = chunk.get_morph_by_case('助詞')
+            for morph in morph_list:
+                tmp_text += morph.base + "\t"
+            subject_dict.update({chunk.get_dst(): chunk.get_num()})
+    for chunk in chunk_list:
+        if chunk.get_dst() == k:
+            for morph in chunk.get_morph_list():
+                tmp_text += morph.surface
+            tmp_text += "\t"
+    tmp_text = tmp_text[:-1]
+
+    print(tmp_text)
---
+++ chapter6/55extract_person_names.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+from pycorenlp import StanfordCoreNLP
+from chapter6.nlp_utils import get_each_sentences
+
+sentence_list = get_each_sentences("nlp.txt")
+nlp = StanfordCoreNLP('http://localhost:9000')
+for sentence in sentence_list:
+    res = nlp.annotate(sentence,
+                   properties={
+                       'annotators': 'tokenize, ssplit, lemma, pos',
+                       'outputFormat': 'json',
+                       'timeout': 1000,
+                   })
+    tokens = res['sentences'][0]['tokens']
+    for token in tokens:
+        if token['pos'] == 'NNP':
+            print(token['word'] + '\t' + token['lemma'] + '\t' + token['pos'])
+
