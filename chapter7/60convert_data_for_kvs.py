2b1a07484a18794b109af3c6a4996cc2276bde51
Koichi Taura <taura.koichi@gmail.com>
2019-03-18 22:02:23 +0900

    [add]convert script from json for redis

---
+++ chapter7/60convert_data_for_kvs.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+import json
+
+artist_dict = {}
+with open("docker/artist.json", "r") as f:
+    for line in f.readlines():
+        tmp_artist_dict = {}
+        data = json.loads(line)
+        if 'area' in data.keys():
+            artist_dict.update({data['name']: data['area']})
+
+with open("docker/redis/artist.txt", "w") as f:
+    for k, v in artist_dict.items():
+        f.write("SET \'" + k + "\' \'" + v + "\'\n")
+
+print(artist_dict)
