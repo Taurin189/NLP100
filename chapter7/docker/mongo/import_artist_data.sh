ba41b35146fd2ed62ccab1e1ec26e58f5a4e0596
Koichi Taura <taura.koichi@gmail.com>
2019-03-18 21:11:42 +0900

    [modify]delete unnecessary comment

--- chapter7/docker/mongo/import_artist_data.sh
+++ chapter7/docker/mongo/import_artist_data.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# cat ./artist.json | while read line
-# do
-#  echo $line > /dev/stdout
-  # mongo --eval "printjson(db.artist.insert($line))"
-# done
 mongoimport --db artist --collection artist --file artist.json
+
